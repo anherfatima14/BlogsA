ab9bf365e6f134d13c9bd8a55f0a8f49fe90a0dc
Anher <anherfatima@gmail.com>
2024-05-22 18:09:43 -0400

    BlogsAPI

---
+++ blogs.api/blogs.api/Controllers/BlogsController.cs
@@ -0,0 +1,26 @@
+﻿using blogs.api.DataAccess;
+using blogs.api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blogs.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly BlogRepository _blogRepository;
+
+        public BlogsController(BlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Blog>>> GetBlogs()
+        {
+            var blogs = await _blogRepository.GetBlogsAsync();
+            return Ok(blogs);
+        }
+    }
+}
---
+++ blogs.api/blogs.api/DataAccess/BlogRepository.cs
@@ -0,0 +1,26 @@
+﻿using blogs.api.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Dapper;
+
+namespace blogs.api.DataAccess
+{
+    public class BlogRepository
+    {
+        private readonly string _connectionString;
+
+        public BlogRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<IEnumerable<Blog>> GetBlogsAsync()
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                var sql = "SELECT * FROM Blogs";
+                return await db.QueryAsync<Blog>(sql);
+            }
+        }
+    }
+}
